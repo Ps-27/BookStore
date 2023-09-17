11624a005e40ccc233ad1f10e23a13a11a08c389
Prity Sinha <71940538+Ps-27@users.noreply.github.com>
2023-09-17 09:54:03 +0530

    Create Welcome.js

---
+++ Frontend/Components/Welcome.js
@@ -0,0 +1,13 @@
+// src/components/Welcome.js
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Welcome to Our Bookstore</h2>
+      {/* Add your sign-in and search components here */}
+    </div>
+  );
+};
+
+export default Welcome;
